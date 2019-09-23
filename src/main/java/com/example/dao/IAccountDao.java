d3f771132b9ae9e6e4edbc166d0ee1804c7c181c
LiC0ng <a1040484@gmail.com>
2019-09-23 22:57:51 +0800

    add account dao interface

--- src/main/java/com/example/dao/IAccountDao.java
+++ src/main/java/com/example/dao/IAccountDao.java
@@ -1,4 +1,24 @@
 package com.example.dao;
 
+import com.example.domain.Account;
+
+import java.util.List;
+
+/**
+ * Account DAO Interface
+ */
 public interface IAccountDao {
+
+    /**
+     * Query all account information
+     * @return The list of account information
+     */
+    public List<Account> findAll();
+
+    /**
+     * Save Account
+     * @param account Account
+     */
+    public void saveAccount(Account account);
+
 }
