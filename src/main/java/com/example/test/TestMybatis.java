48002d1f48f6492c5566473626ca875eb8fb6bad
LiC0ng <a1040484@gmail.com>
2019-09-24 14:30:39 +0800

    test mybatis

--- src/main/java/com/example/test/TestMybatis.java
+++ src/main/java/com/example/test/TestMybatis.java
@@ -14,6 +14,10 @@ import java.util.List;
 
 public class TestMybatis {
 
+    /**
+     * 测试查询
+     * @throws Exception
+     */
     @Test
     public void run1() throws Exception {
         // 加载配置文件
@@ -29,6 +33,32 @@ public class TestMybatis {
         for (Account account : list) {
             System.out.println(account);
         }
+        //关闭资源
+        session.close();
+        in.close();
+    }
+
+    /**
+     * 测试保存
+     * @throws Exception
+     */
+    @Test
+    public void run2() throws Exception {
+        // 加载配置文件
+        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 创建SqlSessionFactory对象
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        // 创建SqlSession对象
+        SqlSession session = factory.openSession();
+        // 获取到代理对象
+        IAccountDao dao = session.getMapper(IAccountDao.class);
+        //保存账户信息
+        Account account = new Account();
+        account.setName("大熊");
+        account.setMoney(10000d);
+        dao.saveAccount(account);
+        //提交事务
+        session.commit();
         //关闭资源
         session.close();
         in.close();
